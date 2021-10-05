900a478a0f3edb33af281f3dcb1e623bbd3cdf55
Tristan Jin <tristanjin@Tristans-MacBook-Pro.local>
2021-10-05 17:28:42 -0400

    first commit

---
+++ EWB_website/frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__body">
+        <p className = "text__header">
+          Hello World! 
+        </p>
+        <p className = "text__header">This is made for EWB</p>
+        <p className = "text__header">Tristan Jin was here</p>
+        <p className = "text__header">More text here</p>
+        <p className = "text__header">More text here</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
