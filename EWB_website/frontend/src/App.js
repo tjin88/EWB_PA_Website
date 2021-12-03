ab742f2e1ecadd92d033a2bb5f457804abe0bfc3
Noah <njina@uwo.ca>
2021-12-03 14:25:49 -0500

    image update

--- EWB_website/frontend/src/App.js
+++ EWB_website/frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <p className = "text__header">
           Hello World! 
         </p>
-        <img src="C:\Users\nj09\OneDrive - The University of Western Ontario\Documents\UWO Year 2\EWB-PA\canada.png" alt="canadian flag">
+        <img src="EWB_PA_Website\EWB_website\frontend\src\images\canada.png" alt="canadian flag">Canada Flag</img>
         <p className = "text__header">This is made for EWB</p>
         <p className = "text__header">Tristan Jin was here</p>
         <p className = "text__header">More text here</p>
@@ -22,4 +22,3 @@ function App() {
 }
 
 export default App;
-//hi
