15056f852f1c11acf93944720ea4c71e4c43608d
Noah <njina@uwo.ca>
2021-12-03 14:52:09 -0500

    actually added the flag properly

--- EWB_website/frontend/src/App.js
+++ EWB_website/frontend/src/App.js
@@ -7,11 +7,11 @@ function App() {
   // The return statement will mainly comprise of HTML
   return (
     <div className="App">
+      <img src="https://lh3.googleusercontent.com/proxy/7s2oXXa75NhmRF3gFhesWpmuk1f1maTp2bLwMGUlC093WOC2o12-II_BAGSbNQjG4tPbc6dAOSISDjORRRrvhNdsnEPyEwTCtG_bVyPX0GMdQniVEytmAWweuHh2EYJZPpFe" alt="canadian flag"/>
       <div className="app__body">
         <p className = "text__header">
           Hello World! 
         </p>
-        <img src="EWB_PA_Website\EWB_website\frontend\src\images\canada.png" alt="canadian flag"/>
         <p className = "text__header">This is made for EWB</p>
         <p className = "text__header">Tristan Jin was here</p>
         <p className = "text__header">More text here</p>
