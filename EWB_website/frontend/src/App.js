5f46d63bfd4877c1bba657eb5237ac808e249c7c
Liam Yoell <lyoell@uwo.ca>
2021-12-03 18:11:32 -0500

    git pul

--- EWB_website/frontend/src/App.js
+++ EWB_website/frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import img from './images/maxresdefault.png';
 function App() {
   // Write js functions here
   
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <img src="https://lh3.googleusercontent.com/proxy/7s2oXXa75NhmRF3gFhesWpmuk1f1maTp2bLwMGUlC093WOC2o12-II_BAGSbNQjG4tPbc6dAOSISDjORRRrvhNdsnEPyEwTCtG_bVyPX0GMdQniVEytmAWweuHh2EYJZPpFe" alt="canadian flag"/>
-      <img src="EWB_website/frontend/src/maxresdefault 1.png" alt = "partyimages"/>
+      <img src= {img} alt = "partyimages"/>
       <div className="app__body">
         <p className = "text__header">
         </p>
