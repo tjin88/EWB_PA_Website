90148e180794392b477da0a1f2a14ab0cf8685d2
Liam Yoell <lyoell@uwo.ca>
2021-12-03 15:14:37 -0500

    added PNG

--- EWB_website/frontend/src/App.js
+++ EWB_website/frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <img src="https://lh3.googleusercontent.com/proxy/7s2oXXa75NhmRF3gFhesWpmuk1f1maTp2bLwMGUlC093WOC2o12-II_BAGSbNQjG4tPbc6dAOSISDjORRRrvhNdsnEPyEwTCtG_bVyPX0GMdQniVEytmAWweuHh2EYJZPpFe" alt="canadian flag"/>
+      <img src="EWB_website/frontend/src/maxresdefault 1.png" alt = "partyimages"/>
       <div className="app__body">
         <p className = "text__header">
         </p>
