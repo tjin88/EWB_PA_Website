eae1328411495556950119154c3b65596e4f83d0
Tristan Jin <tjin368@gmail.com>
2022-01-03 11:34:28 -0500

    removed unused files

--- EWB_website/frontend/src/index.js
+++ EWB_website/frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import QuestionPage from './pages/QuestionPage';
