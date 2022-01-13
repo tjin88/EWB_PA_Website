9e5a4096e0ed672b2fad2fe8fe169545339f483f
Tristan Jin <tjin368@gmail.com>
2022-01-13 08:27:50 -0500

    added questions and HomePage image link

--- EWB_website/frontend/src/pages/HomePage.js
+++ EWB_website/frontend/src/pages/HomePage.js
@@ -5,14 +5,18 @@ import parties from '../images/parties.png';
 // import parties2 from '../images/parties2.png';
 
 function HomePage() {
+  const url = "https://www.google.com/search?gs_ssp=eJzj4tFP1zcsNM2qMKkwSTZg9BLMSK1UKMlILUpVSEnNycxJzAAArRcK1Q&q=hey+there+delilah&rlz=1C5CHFA_enCA709CA710&oq=hey+&aqs=chrome.1.69i57j46i433i512l3j0i512j46i175i199i512j46i433i512j0i433i512j0i512j0i271.2885j0j7&sourceid=chrome&ie=UTF-8";
 
   // The return statement will mainly comprise of HTML
   return (
     <div className="HomePage">
       <div className="HomePage__body">
       <img className = "canadianFlagRight" src = {canadianFlag} alt = "Canadian Flag"/>
-
-      <img className = "parties" src = {parties} alt = "Canadian Parties"/>
+      
+      <a id="hdimg" href={url} target="-blank">
+            {/* <img id="image_of_the_day" src="${innerData.url}" alt="image-by-nasa"/> */}
+            <img className = "parties" src = {parties} alt = "Canadian Parties"/>
+      </a>
       {/* <div class="rectangle"></div>
       <img className = "parties2" src = {parties2} alt = "Canadian Parties"/> */}
 
--- EWB_website/frontend/src/pages/QuestionPage.js
+++ EWB_website/frontend/src/pages/QuestionPage.js
@@ -1,7 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './QuestionPage.css';
 
-var Questions = ["Q1","Q2","Q3","Q4"]
+var Questions = ["Canada should invest in greater pipeline infrastructure",
+"It is important that the government subsidizes the development and implementation of environmentally friendly technology",
+"The government needs to take stronger action (carbon taxes, etc) to curb emissions that contribute to climate change",
+"Canada should subsidize child care for children until they turn 18.",
+"The government should fully subsidize the cost of post-secondary education for Canadian citizens.",
+"The government should intervene in the housing market and regulate costs so it is more affordable.",
+"Minimum wage should be increased in accordance with living costs and inflation.",
+"The Federal government should move forward in implementing a nationalized universal pharmacare program to cover the cost of prescription drugs for all Canadians",
+"The Canadian government should not allow essential services/infrastructure to be privatized (roads, utilities, Wi-Fi, public transportation, etc).",
+"Canada should increase tariffs on foreign goods to protect their own domestic industries.",
+"The government should increase taxes on ultra-wealthy companies and individuals, and lower taxes on the poor.",
+"Canada should escalate their actions against North Korea and Iran to prevent them from developing nuclear weapons.",
+"Canada should ban TikTok and Huawei to prevent them from selling Canadians’ data to China.",
+"Canada should reduce the scope of policing and involve other services (ie. mental health centres) in non-violent situations.",
+"Canada should increase their funding of the United Nations to help promote peace and democracy.",
+"Canada should take a harsher stance against China, especially regards to their human rights violations.",
+"Canada should stop all trade with authoritarian countries or countries that have a poor human rights record.",
+"Canada needs encourage more immigration and lower the barriers for new immigrants looking to move to Canada",
+"Canada needs to invest more money into indigenous communities/reserves",
+"Canada should allow Indigenous peoples to have their own system of government.",
+"Canada should be doing more to distribute COVID-19 vaccinations to poorer nations",
+"Canada’s Covid policies have gone too far, restricting Canadians personal freedoms",]
 var Answers = []
 
 function QuestionPage() {
